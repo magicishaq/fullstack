247075f9dd791d82f584258c6465c3985595f0c5
magicishaq <ishaqkhan1993@gmail.com>
2021-07-14 15:16:57 +0100

    added a server to bypass the cors

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+const urls = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json'; 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/photos/random', (req, res) => {
+  request(
+    { url: urls },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import {useState, useEffect} from 'react'; 
+import './App.scss';
+const api = '6a2bc1233f7854d7a66b3b365c3679fd'; 
+const secret = '6fc93a132e08a7b2'; 
+
+const App = () =>  {
+const [flickr, setFlickr] = useState({})
+
+const getImage = async function () {
+  const url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json'; 
+  const result = await fetch(url,{ method: 'GET',
+  mode: 'cors',
+});
+  debugger;  
+  const json = await result.json();
+  debugger;  
+  setFlickr(json); 
+}
+
+useEffect(() => getImage(), []); 
+
+  const image = ''
+  const photoTitle = ''
+  const author = ''
+  const description = '' 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="photo">
+        <img src={image} alt={photoTitle} />
+        </div> 
+
+      <div className="content">
+<h1> {photoTitle}  {author} </h1>
+<p>{description} </p>
+      </div>
+
     </div>
   );
 }
